95f78f623a1e9f76503bcfe19a76a1ddc30a779e
Suman_Saurabh_Das <dsumansaurabh@gmail.com>
2022-06-25 12:40:10 +0530

    CWH Video 5 Completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';   // Importing css file App.css
 
-let name = "Alex";
 function App() {
   return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contacts</li>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">TextUtils</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbar">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">About</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
       </nav>
-      <h1>Hello {name}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta officia culpa dolor quidem impedit aperiam ducimus vel, voluptatum iusto blanditiis, commodi quis est aliquam at cupiditate nisi ab animi nulla! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit pariatur, similique excepturi unde ipsam adipisci. Odit esse deserunt aut unde tempore at incidunt dolore dicta, voluptatem illum, asperiores culpa atque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet quos quaerat doloremque aliquid, nulla aperiam consequatur suscipit dolore, maxime libero iure, blanditiis ad? Aperiam iusto tempora quis officiis, quo eius.</p>
     </>
   );
 }
